837e480f0cc4799eb0f0c57e95728337a8cc6933
kerogs <luckywies@gmail.com>
2024-08-22 00:43:23 +0200

    main add

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # MultiDownload
 local website multi platform video downloader
+
+## Installation
+1. clone repository
+```sh
+git clone https://github.com/kerogs/MultiDownload.git
+```
+
+2. installation packagist dependence
+```sh
+composer i
+```
+
+3. installation NPM
+```sh
+npm i
+```
+
+## Packagist
+|Nom|Url|Utilisation|
+|-|-|-|
+|youtube-downloader|https://packagist.org/packages/athlon1600/youtube-downloader|youtube video|
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MultiDownload</title>
+    <link rel="stylesheet" href="./dist/css/style.css">
+    <link rel="stylesheet" href="./node_modules/boxicons/css/boxicons.min.css">
+</head>
+
+<body>
+
+    <div class="ccenter">
+        <h1><i class='bx bx-cloud-download' ></i> MultiDownloader</h1>
+        <form action="catch.php" method="post">
+            <label for="" class="group">
+                <i class='bx bx-question-mark'></i>
+                <input type="url" name="url" id="url" placeholder="https://xxx.yy/zzz" autofocus>
+            </label>
+            <button type="submit" class="ok">Download</button>
+        </form>
+    </div>
+
+</body>
+
+</html>
